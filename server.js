1a6be904e1556ad6aaf75b7d3097db7b8f45feaa
TanHung189 <hung189204@gmail.com>
2025-09-22 00:38:09 +0700

    Add UI index, css, bootstrap

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { loadTasks, saveTasks } = require("./tasks");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
